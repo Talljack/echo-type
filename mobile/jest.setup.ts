7d216c321b305440f04ffbf6e3eacc56589588b7
Yugang Cao <yugang.cao12@gmail.com>
2026-04-14 08:10:10 +0800

    test: restore mobile paper shim

--- mobile/jest.setup.ts
+++ mobile/jest.setup.ts
@@ -11,3 +11,28 @@ jest.mock('expo-router', () => ({
   }),
   useLocalSearchParams: () => ({}),
 }));
+
+jest.mock('react-native-paper', () => {
+  const { Text } = require('react-native');
+
+  return {
+    Text,
+  };
+});
+
+jest.mock('react-test-renderer', () => {
+  const actual = jest.requireActual('react-test-renderer');
+
+  return {
+    ...actual,
+    create(element, options) {
+      let instance;
+
+      actual.act(() => {
+        instance = actual.create(element, options);
+      });
+
+      return instance;
+    },
+  };
+});
--- mobile/src/components/ui/MvpNoticeCard.tsx
+++ mobile/src/components/ui/MvpNoticeCard.tsx
@@ -1,5 +1,5 @@
 import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native';
+import { Text } from 'react-native-paper';
 
 interface MvpNoticeCardProps {
   title: string;
